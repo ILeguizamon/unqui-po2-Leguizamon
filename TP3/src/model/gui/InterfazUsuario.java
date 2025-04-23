1d13d58a7be9247b8b0309dce30bffbdae2dbca3
Ivan Leguizamon <ivanleguizamon939@gmail.com>
2025-04-23 00:49:06 -0300

    Eliminar archivos ignorados según .gitignore

--- TP3/src/model/gui/InterfazUsuario.java
+++
@@ -1,7 +0,0 @@
-package model.gui;
-
-public class InterfazUsuario {
-    public void mostrarMensaje() {
-        System.out.println("Este es un mensaje de la interfaz de usuario.");
-    }
-}
--- TP3/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module TP3_INTRO_JAVA_IvanLeguizamon {
-	requires org.junit.jupiter.api;
-}
