5a75f72829b5c375d1b6c5bc51048131ab0f2a08
Ivan Leguizamon <ivanleguizamon939@gmail.com>
2025-05-28 16:04:19 -0300

    Nuevos cambios

--- TP-Composite/src/ar/edu/unq/po2/tp7/composite/juegoDeEstrategia/Caballero.java
+++ TP-Composite/src/ar/edu/unq/po2/tp7/composite/juegoDeEstrategia/Caballero.java
@@ -8,28 +8,25 @@ public class Caballero extends Soldado {
 
 	@Override
 	public void moverA(Punto otroPunto) {
-	    // Mientras la ubicación no sea igual al destino, mover paso a paso
+
 	    while (!(this.ubicacion.equals(otroPunto))) {
-	        // Si no estamos en el destino en X, moveremos en X
+
 	        if (this.ubicacion.getX() != otroPunto.getX()) {
 	            this.moverseUnaUbicacionEnX(otroPunto);
 	        }
 
-	        // Si no estamos en el destino en Y, moveremos en Y
 	        if (this.ubicacion.getY() != otroPunto.getY()) {
 	            this.moverseUnaUbicacionEnY(otroPunto);
 	        }
 	    }
 	}
 
-	// Método para mover en el eje X, por implementar (por ejemplo, en zigzag)
 	private void moverseUnaUbicacionEnX(Punto destino) {
-	    // Lógica de caminata en zigzag (por implementar)
+		// caminata en zigzag en eje X
 	}
 
-	// Método para mover en el eje Y, por implementar (por ejemplo, en zigzag)
 	private void moverseUnaUbicacionEnY(Punto destino) {
-	    // Lógica de caminata en zigzag (por implementar)
+		 // caminata en zigzag en eje Y
 	}
 
 }
--- TP-Composite/src/ar/edu/unq/po2/tp7/composite/juegoDeEstrategia/Ingeniero.java
+++ TP-Composite/src/ar/edu/unq/po2/tp7/composite/juegoDeEstrategia/Ingeniero.java
@@ -35,11 +35,7 @@ public class Ingeniero extends Soldado {
 	}
 	
 	public void moverseUnaUbicacionMasCercaDe(Punto destino) {
-	    int dx = Integer.compare(destino.getX(), this.ubicacion.getX()); // -1, 0 o 1
-	    int dy = Integer.compare(destino.getY(), this.ubicacion.getY()); // -1, 0 o 1
-
-	    this.ubicacion.setX(this.ubicacion.getX() + dx);
-	    this.ubicacion.setY(this.ubicacion.getY() + dy);
+		 // caminata en diagonal, moviéndose 1 unidad en X y/o Y hacia el destino
 	}
 
 
